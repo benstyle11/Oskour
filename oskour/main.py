2a803ee25b36c05c21a63a844d360d650bb2f270
Antoine Bendimerad-Hohl <antoine.bendimerad@icloud.com>
2025-01-11 23:49:00 +0100

    Add basic main function

---
+++ oskour/main.py
@@ -0,0 +1,20 @@
+import sys
+
+from oskour.dataStructure  import DataConv, ResultatsConv
+from oskour.ioFunc import save_to_file, import_data
+from oskour.convSolver import solve
+from oskour.postProcessing import displayConv
+
+def import_solve_export(input_path:str,output_path:str, custom_constraints = []):
+    dataConv:DataConv = import_data(input_path)
+    resConv:ResultatsConv = solve(dataConv,custom_constraints)
+    displayConv(resConv)
+    save_to_file(resConv)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2:
+        input_path = sys.argv[1]
+    if len(sys.argv) >= 3:
+        output_path = sys.argv[2]
+    import_solve_export(input_path,output_path)
