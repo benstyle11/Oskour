2b3358b60ca6fa49ffbcb84511a90f985db3a45c
Antoine Bendimerad-Hohl <antoine.bendimerad@icloud.com>
2025-01-11 23:56:12 +0100

    corrects name of the main file

--- oskour/__main__.py
+++ oskour/__main__.py
@@ -11,10 +11,12 @@ def import_solve_export(input_path:str,output_path:str, custom_constraints = [])
     displayConv(dataConv,resConv)
     save_to_file(dataConv,resConv)
 
-
-if __name__ == "__main__":
+def main():
     if len(sys.argv) >= 2:
         input_path = sys.argv[1]
     if len(sys.argv) >= 3:
         output_path = sys.argv[2]
     import_solve_export(input_path,output_path)
+
+if __name__ == "__main__":
+    main()
